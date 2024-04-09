866018b0b379344c4cc7cda28799e8a51c92ede0
Nuelli <emmanuelabwao@gmail.com>
2024-04-09 21:43:22 +0300

    added eslint configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     });
 
+    module.exports = {
+      reactStrictMode: true,
+      eslint: {
+        // Warning: Disabling rules here can affect code quality across the project
+        ignoreDuringBuilds: true, // Optional: Ignore ESLint errors during build
+        rules: {
+          "react/no-unescaped-entities": "off", // Disables the rule
+        },
+      },
+    };
+    
+
     return config;
   },
 }
